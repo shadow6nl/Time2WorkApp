a7262cdbba3da2a07d31cc0c41b163e1698fdde6
Bas Buvelot <s.f.buvelot@gmail.com>
2018-06-04 19:18:30 +0200

    First time use setup

--- Time2WorkApp/Time2WorkApp/App.xaml.cs
+++ Time2WorkApp/Time2WorkApp/App.xaml.cs
@@ -2,18 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Time2WorkApp.Helpers;
 using Xamarin.Forms;
 
 namespace Time2WorkApp
 {
 	public partial class App : Application
-	{
+	{   
+        public string IsFirstTime
+        {
+            get { return Settings.GeneralSettings; }
+            set
+            {
+                if (Settings.GeneralSettings == value)
+                    return;
+
+                Settings.GeneralSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
 		public App ()
 		{
 			InitializeComponent();
-
-			MainPage = new NavigationPage(new MainPage());
+            //check if the app is running for the first time
+            if (IsFirstTime == "yes")
+            {
+                //if this is the first time, set it to "No" and load the
+                // mainpage, wich will show at the first time use
+                IsFirstTime = "no";
+                MainPage = new Time2WorkApp.OptionsPage();
+            }
+            else
+            {
+                // If this is not the first time,
+                // Go to the login page
+                MainPage = new NavigationPage(new MainPage());
+            }
 		}
 
 		protected override void OnStart ()
