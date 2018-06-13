c9753f91c23e2e84ef5727759be346e7e0b3b5d4
Devildanger <0941224@hr.nl>
2018-06-13 10:43:24 +0200

    new NewPasswordPage

--- Time2WorkApp/Time2WorkApp/NewPAsswordPage.xaml.cs
+++ Time2WorkApp/Time2WorkApp/NewPAsswordPage.xaml.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Time2WorkApp.Helpers;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Time2WorkApp
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class NewPasswordPage : ContentPage
-    {
-        public NewPasswordPage()
-        {
-            //Geeft error als ik uncomment
-
-            //InitializeComponent();
-        }
-
-        private void NewPasswordSaveButton_Clicked(object sender, EventArgs e)
-        {
-            // save to database logic
-        }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPasswordPage : ContentPage
+	{
+		public NewPasswordPage ()
+		{
+			InitializeComponent ();
+		}
     }
+    //private void newpasswordsavebutton_clicked(object sender, eventargs e)
+    //{
+    //    save to database logic
+    //}
 }
