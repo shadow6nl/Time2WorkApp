c7c5ebe5b024f3961df527b0cc25c18911faa8c6
Devildanger <0941224@hr.nl>
2018-06-13 11:33:00 +0200

    popups Newpasswordpage

---
+++ Time2WorkApp/Time2WorkApp/NewPasswordPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Time2WorkApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPasswordPage : ContentPage
+	{
+		public NewPasswordPage ()
+		{
+		   InitializeComponent ();
+		}
+
+        private void NewPasswordSaveButton_Clicked(object sender, EventArgs e)
+        {
+            bool isoldPasswordEmpty  = string.IsNullOrEmpty(oldPassword.Text);
+            bool isnewPassword1Empty = string.IsNullOrEmpty(newPassword1.Text);
+            bool isnewPassword2Empty = string.IsNullOrEmpty(newPassword2.Text);
+
+            bool isoldPasswordinDB   = string.IsNullOrEmpty(oldPassword.Text);
+            bool isnewPassword1inDB  = string.IsNullOrEmpty(newPassword1.Text);
+            bool isnewPassword2inDB  = string.IsNullOrEmpty(newPassword2.Text);
+
+
+            if (isoldPasswordEmpty || isnewPassword1Empty || isnewPassword2Empty)
+            {
+                DisplayAlert("Waarschuwing", "Er is nog een tekstvak leeg.", "OK");
+            }
+
+
+
+            else
+                DisplayAlert("Waarschuwing", "Uw wachtwoord is opgeslagen", "OK");
+        }
+    }
+}
