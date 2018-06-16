31db1f43cfaa12154c1617bd45d52e0c656d33c2
Devildanger <0941224@hr.nl>
2018-06-16 11:46:27 +0200

    Database connection stable (Mainactivity.cs)

---
+++ Time2WorkApp/Time2WorkApp.Android/Model/Pauzes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time2WorkApp.UWP.Model
+{
+    class Pauzes
+    {
+        public int id { get; set; }
+
+        public string email { get; set; }
+
+        public string wachtwoord { get; set; }
+
+        public float brutoloon { get; set; }
+    }
+}
---
+++ Time2WorkApp/Time2WorkApp/Model/Activiteit.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time2WorkApp.Model
+{
+    [Table("Activiteiten")]
+    public class Activiteit //something
+    {
+        [PrimaryKey]
+        public int id { get; set; }
+
+        public DateTime datum { get; set; }
+
+        public string activiteit { get; set; }
+
+        public DateTime startTijd { get; set; }
+
+        public DateTime stopTijd { get; set; }
+
+        public DateTime totaleTijd { get; set; }
+    }
+}
+
+
