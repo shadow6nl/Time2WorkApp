70481e7b9d1bd7abb96f503bf27f7348669e0a46
Devildanger <0941224@hr.nl>
2018-06-17 23:10:38 +0200

    Overzichtpage nooit lege DB

--- Time2WorkApp/Time2WorkApp/SummaryPage.xaml.cs
+++ Time2WorkApp/Time2WorkApp/SummaryPage.xaml.cs
@@ -21,19 +21,17 @@ namespace Time2WorkApp
 		public SummaryPage ()
 		{
             InitializeComponent();
-            //refreshDePaginaDatabase();
+
+            if (dbContext.db.Table<Activiteit>().FirstOrDefault() == null)
+            {
+                dbContext.Insert_Activity_Into_Table(new Activiteit { id = 1, activiteit = "Er zijn nog geen activiteiten opgeslagen." });
+            }
+
+            refreshDePaginaDatabase();
         }
 
         public void refreshDePaginaDatabase()
         {
-            
-        }
-
-
-        private void Update_Clicked(object sender, EventArgs e)
-        {
-            //refreshDePaginaDatabase();
-
             current_activity = dbContext.db.Table<Activiteit>().Last();
             int last_index_activiteiten1 = current_activity.id;
             int last_index_activiteiten2 = last_index_activiteiten1 - 1;
@@ -86,6 +84,12 @@ namespace Time2WorkApp
             {
                 Activiteit10.Text = dbContext.db.Table<Activiteit>().FirstOrDefault(x => x.id == last_index_activiteiten10).activiteit;
             }
+        }
+
+
+        private void Update_Clicked(object sender, EventArgs e)
+        {
+            refreshDePaginaDatabase();
         }
 
         
